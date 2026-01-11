019c67fa90bf76dd177bb3a2c9adeee97c59e1c2
Manisha Bhatta <manishabhatta11@gmail.com>
2026-01-11 20:08:03 +0530

    Add Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home.jsx";
 import { Navbar } from "./components/Navbar.jsx"; // import Navbar
 import { SkillsSection } from "./components/SkillsSection.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
       </Routes>
       <Analytics />
+      <SpeedInsights />
 
     </BrowserRouter>
 
